10c9248ac242aebb10905d8c87fa72251839b868
asdetmin <alexanderpurkhalo@gmail.com>
2016-05-13 09:02:21 -0400

    New Post

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 
 // Components
 import App from './components/app';
-// 3. To make 'PostIndex' component active we import it
 import PostsIndex from './components/post_index';
+import PostsNew from './components/posts_new';
 
-// Paths
-// 2. Here we add a new 'PostIndex' component as IndexRoute to make its as root route
 export default (
     <Route path="/" component={App} >
         <IndexRoute component={PostsIndex} />
+        <Route path="posts/new" component={PostsNew} />
     </Route>
 );
